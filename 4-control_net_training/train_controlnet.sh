577f8b8b101283b88436089df9731d159c7c31fe
MrPio <valeriomorelli50@gmail.com>
2025-05-19 19:48:25 +0200

    Add validation set in training shell script

--- 4-control_net_training/train_controlnet.sh
+++ 4-control_net_training/train_controlnet.sh
@@ -1,14 +1,18 @@
 export MODEL_DIR="stable-diffusion-v1-5/stable-diffusion-v1-5"
 export OUTPUT_DIR="/leonardo_scratch/fast/IscrC_MACRO/Texture-Anything/4-control_net_training/output"
 export CACHE_DIR="/leonardo_scratch/fast/IscrC_MACRO/Texture-Anything/.huggingface"
+export DATASET_DIR="/leonardo_scratch/fast/IscrC_MACRO/Texture-Anything/4-control_net_training/dataset"
 
-accelerate launch train_controlnet.py \
- --pretrained_model_name_or_path=$MODEL_DIR \
- --output_dir=$OUTPUT_DIR \
- --cache_dir=$CACHE_DIR \
- --dataset_name=fusing/fill50k \
- --resolution=512 \
- --learning_rate=1e-5 \
- --validation_image "./conditioning_image_1.png" "./conditioning_image_2.png" \
- --validation_prompt "red circle with blue background" "cyan circle with brown floral background" \
- --train_batch_size=4
+accelerate launch --mixed_precision="fp16" --multi_gpu train_controlnet.py \
+    --pretrained_model_name_or_path=$MODEL_DIR \
+    --output_dir=$OUTPUT_DIR \
+    --cache_dir=$CACHE_DIR \
+    --train_data_dir=$MODEL_DIR \
+    \
+    --resolution=512 \
+    --learning_rate=1e-5 \
+    --train_batch_size=4 \
+    --mixed_precision="fp16" \
+    \
+    --validation_image $(printf "\"%s\" " dataset/validation/uv/*.png) \
+    --validation_prompt python3 -c 'import csv; print(" ".join(f"\"{row[2]}\"" for row in csv.reader(open("dataset/validation/metadata.csv")) if row != [] and row[0] != "uv_file_name"))'
